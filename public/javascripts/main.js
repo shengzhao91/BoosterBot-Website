6ecee1ec1a6dfb94960932a841bf028a3a6bb76b
Sheng Zhao <zhsh19912004@gmail.com>
2014-07-27 23:09:29 -0500

    Video Popup
    Video is played through Bootstrap Modal. Auto play and stop. Minor look
    clean up.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,61 @@
 ;(function(){
-
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
-
-
+	// Menu settings
+	$('#menuToggle, .menu-close').on('click', function(){
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
 })(jQuery)
+
+$( document ).ready(function() {
+	$('#watchVideoBtn').click(function(){
+		console.log('Playing Intro Video');
+		window.introvideoplayer.playVideo();
+	});
+
+	$('#FirmataDemoModal').on('hidden.bs.modal', function () {
+	    // do something…
+	    introvideoplayer.stopVideo();
+	})
+});
+
+var introvideoplayer;
+function onYouTubeIframeAPIReady() {
+	console.log( "iFrame ready!" );
+	introvideoplayer = new YT.Player('introvideo', {
+		height: '390',
+		width: '640',
+		videoId: 'RDHdkwH2jVs',
+		playerVars: { 
+			'autoplay': 0, // auto start
+			'autohide': 1, // auto hide control after play
+			'rel': 0, // no related video
+			'modestbranding' : 1, // hides YouTube icon
+			 },
+		events: {
+			// 'onReady': onPlayerReady,
+			// 'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+					// <iframe id="player" type="text/html" width="640" height="390"
+					//   src="http://www.youtube.com/embed/RDHdkwH2jVs?enablejsapi=1&origin=localhost:3000"
+					//   frameborder="0"></iframe>
+
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+	console.log('state changed: ');
+
+	// if (event.data == YT.PlayerState.PLAYING && !done) {
+	// 	setTimeout(stopVideo, 6000);
+	// 	done = true;
+	// }
+}
+function stopVideo() {
+	introvideoplayer.stopVideo();
+}
